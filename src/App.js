3d5aeff0579c4cfb9cda60e0ad991ec6117d2c24
michaelp <michaelp@linux-laptop.lan>
2023-11-17 18:50:49 +0000

    added thumbs

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 
 function App() {
 
-  const [birds, setBirds] = useState([]);
+  const [birds, setBirds] = useState([{}]);
   const FILE_STORE = "http://area2-documentsstore.s3-website.eu-west-2.amazonaws.com/";
   const FILE_STORE_MAP = "https://area2-maps.s3.eu-west-2.amazonaws.com/data.json";
 
@@ -18,10 +18,11 @@ function App() {
         data.images.filter(
           (e) => e.name.toLowerCase().endsWith(".jpg") || 
                  e.name.toLowerCase().endsWith(".jpeg")).forEach((e) => {
+          const thumb = `${FILE_STORE}thumbs/thumb${e.name}`;
           const path = `${FILE_STORE}${e.name}`;
           const imageElement = new Image();
           imageElement.src = path;
-          temp.push(path);
+          temp.push({path,thumb});
         })
         setBirds(temp);
       })
@@ -39,7 +40,7 @@ function App() {
       </header>
       <section className='flex-container'>
         {birds.map((p, i) => 
-        <div key={i} className="image-container"><a href={p}><img src={p} alt='Bird' /></a></div>
+        <a href={p.path}><img src={p.thumb} alt='Bird' /></a>
         )}
       </section>
       <footer>
